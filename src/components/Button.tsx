2cf95655e83199c1e54791adafcd63119f4fa824
liviakobayashii <liviakmm10@gmail.com>
2025-02-20 12:05:26 -0300

    style: melhoria na estilização da página de cadastro e login

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,10 @@ export default function Button({
   ...props
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className="border border-white p-3 rounded-md self-end" {...props}>
+    <button
+      className="bg-blue-700 hover:bg-blue-900 duration-200 border border-white p-3 rounded-md self-end"
+      {...props}
+    >
       Enviar
     </button>
   );
