72bbf41d3b87ffb74c728d2b65652b09b1acf84e
liviakobayashii <liviakmm10@gmail.com>
2025-03-13 15:27:54 -0300

    fix: exclusão do ";" no código

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {children} <Toaster />;
+          {children} <Toaster />
         </Providers>
       </body>
     </html>
