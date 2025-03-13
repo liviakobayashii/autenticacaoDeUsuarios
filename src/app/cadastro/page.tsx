a530b4d13746f409f19bcccb42c8aeabea0e8d6d
liviakobayashii <liviakmm10@gmail.com>
2025-03-13 14:22:16 -0300

    update: atualização e otimização do código

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,23 +1,55 @@
 "use client";
-import Button from "@/components/Button";
+// import Button from "@/components/Button";
 import { AuthContext } from "@/contexts/AuthContext";
 import { redirect, useRouter } from "next/navigation";
-import { useContext, useState } from "react";
-
-type User = {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-};
+import { useContext } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function SignIn() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const router = useRouter();
 
-  const userCtx = useContext(AuthContext);
+  const formSchema = z.object({
+    name: z.string().trim().min(4, "O nome precisa ter no mínimo 4 letras."),
+    email: z.string().email({
+      message: "E-mail inválido. Verifique novamente",
+    }),
+    password: z
+      .string()
+      .min(2, {
+        message: "A senha precisa ter no mínimo 2 caracteres",
+      })
+      .max(12),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
   const getUsers = () => {
     const storedUsers = localStorage.getItem("@Users");
@@ -25,30 +57,27 @@ export default function SignIn() {
     return storedUsers ? JSON.parse(storedUsers) : [];
   };
 
-  const router = useRouter();
-
-  const addUser = () => {
-    if (name.trim() !== "" && email.trim() !== "" && password.trim() !== "") {
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (
+      values.name.trim() !== "" &&
+      values.email.trim() !== "" &&
+      values.password.trim() !== ""
+    ) {
       const users = getUsers();
 
-      const emailExists = users.some((item: User) => item.email === email);
+      const emailExists = users.some(
+        (item: any) => values.email === item.email
+      );
 
-      if (emailExists) {
-        setError("Esse email já foi cadastrado. Faça o Login");
-        return;
-      }
-
-      setError("");
-      const id = users.length + 1;
-      const newUser = { id, name, email, password };
-      users.push(newUser);
-      localStorage.setItem("@Users", JSON.stringify(users));
+      // if (emailExists) {
+      //   console.log("não pode adicionar");
+      // } else {
+      users.push(values);
+      // localStorage.setItem("@Users", JSON.stringify(users));
       router.push("/login");
+      toast("Cadastro efetuado com sucesso! Agora faça o login.");
+      // }
     }
-  };
-
-  if (userCtx?.user) {
-    redirect("/dashboard");
   }
 
   return (
@@ -56,57 +85,102 @@ export default function SignIn() {
       <div className="w-screen h-screen bg-slate-200 items-center justify-center">
         <img src="../../../signin.png" alt="" className="h-screen w-screen" />
       </div>
-
       <div className="flex flex-col h-screen w-screen bg-blue-600 justify-center items-center">
-        <form
-          className="flex flex-col bg-slate-200 border border-white/30 w-96 h-auto rounded-md p-5 gap-3 justify-center items-center "
-          id="signInId"
-        >
-          {error && (
-            <div className="bg-yellow-200 text-black border border-yellow-600 rounded-md p-3 mb-4">
-              {error}
-            </div>
-          )}
-
-          <h1 className="font-bold text-black text-2xl mb-3">
-            Faça seu cadastro!
-          </h1>
-          <input
-            value={name}
-            type="text"
-            placeholder="Nome completo"
-            className="w-full p-3 rounded-md bg-white/90 outline-none text-black border border-gray-400"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            value={email}
-            type="email"
-            placeholder="exemplo@email.com"
-            className="w-full p-3 rounded-md bg-white/90 outline-none text-black border border-gray-400"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            value={password}
-            type="password"
-            placeholder="*****"
-            className="w-full p-3 rounded-md bg-white/90 outline-none text-black border border-gray-400"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type="button" onClick={addUser} />
-          <p
-            className="text-black/60 mt-4 text-sm"
-            onClick={() => router.push("/login")}
-          >
-            Já tem uma conta? Então faça o{" "}
-            <span className="cursor-pointer hover:text-blue-700 duration-200 ">
-              Login
-            </span>{" "}
-            agora mesmo!
-          </p>
-        </form>
+        <Card className="w-96 border h-auto">
+          <CardHeader>
+            <CardTitle className="text-3xl text-blue-600">
+              Área de cadastro
+            </CardTitle>
+            <CardDescription>Faça seu cadastro agora mesmo!</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome completo</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite seu nome completo"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>E-mail</FormLabel>
+                      <FormControl>
+                        <Input placeholder="exemplo@email.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Senha</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Digite sua senha"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Cadastro</Button>
+                <p className="text-black/60 mt-4 text-sm">
+                  Já tem uma conta? Então faça o{" "}
+                  <span
+                    className="cursor-pointer hover:text-blue-700 duration-200 "
+                    onClick={() => router.push("/login")}
+                  >
+                    Login
+                  </span>{" "}
+                  agora mesmo!
+                </p>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
       </div>
     </section>
   );
-  // return (
-  // );
 }
+
+//     const emailExists = users.some((item: User) => item.email === email);
+
+//     if (emailExists) {
+//       setError("Esse email já foi cadastrado. Faça o Login");
+//       return;
+//     }
+
+//     const id = users.length + 1;
+//     const newUser = { id, name, email, password };
+//     users.push(newUser);
+//     localStorage.setItem("@Users", JSON.stringify(users));
+//     router.push("/login");
+//   }
+// };
+
+// if (userCtx?.user) {
+//   redirect("/dashboard");
+
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import SignIn from "./cadastro/page";
+// import SignIn from "./cadastro/page";
 
 export default function Page() {
   return (
-    <div className="flex flex-col h-screen w-screen justify-center items-center">
-      <SignIn />
-    </div>
+    <div className="flex flex-col h-screen w-screen justify-center items-center"></div>
   );
 }
