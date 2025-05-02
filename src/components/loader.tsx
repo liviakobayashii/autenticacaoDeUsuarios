c0892abf51460ea533b009ca1f4890d48f78e276
liviakobayashii <liviakmm10@gmail.com>
2025-05-01 23:46:33 -0300

    feat: criação do loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,10 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function Loader() {
+  return (
+    <Icon
+      icon="mingcute:loading-fill"
+      className="text-blue-600 animate-spin size-12"
+    />
+  );
+}
